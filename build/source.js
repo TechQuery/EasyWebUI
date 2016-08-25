4034cde700cc04572445667b255cb52c9b306d39
Tech_Query <shiy007@qq.com>
2016-08-25 17:18:57 +0800

    【新增】.Border-Column、.Border-Row 网格边框实用类；
    【优化】3 处细节问题

--- build/source.js
+++ build/source.js
@@ -2,18 +2,17 @@
     name:            'EasyWebUI',
     baseUrl:         '../source',
     paths:           {
-        jquery:       'http://cdn.bootcss.com/jquery/1.12.3/jquery',
-        'jQuery+':    'http://git.oschina.net/Tech_Query/iQuery/raw/master/jQuery+.js',
-        'iQuery+':    'http://git.oschina.net/Tech_Query/iQuery/raw/master/iQuery+.js'
+        jquery:       'http://cdn.bootcss.com/jquery/1.12.4/jquery',
+        'jQuery+':    'http://tech_query.oschina.io/iquery/jQuery+',
+        'iQuery+':    'http://tech_query.oschina.io/iquery/iQuery+'
     },
     out:             '../EasyWebUI.js',
     onBuildWrite:    function () {
         var fParameter = 'BOM, DOM',  aParameter = 'self,  self.document';
 
-        if (arguments[0] != 'extension/ES-5') {
-            fParameter += ', $';
-            aParameter += ',  self.jQuery || self.Zepto';
-        }
+        fParameter += ', $';
+        aParameter += ',  self.jQuery || self.Zepto';
+
         return arguments[2]
             .replace(/^define[\s\S]+?(function \()[^\)]*/m,  "\n($1" + fParameter)
             .replace(/\s+var BOM.+?;/, '')
